22f8a4ad34ca13a1268f937f064da7e1872f714c
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-10-25 22:14:30 +0000

    Add function to return tail of an array & check original is unaltered

---
+++ tail.js
@@ -0,0 +1,21 @@
+// Check if assertion passed or failed by printing a message
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Return an array without the first element
+const tail = (array) => {
+  return array.slice(1);
+};
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+console.log(tail(['bob']));
+console.log(tail([]));
