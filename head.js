a74f7c90709b2124177373786c33c7bbc0645520
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-10-25 21:43:21 +0000

    Add function that returns the first item in the array

---
+++ head.js
@@ -0,0 +1,17 @@
+// Check if assertion passed or failed by printing a message
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = (array) => {
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([6]), 6);
+assertEqual(head([]), undefined);
