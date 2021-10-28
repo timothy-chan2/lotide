231b3ba202215f7e0878e09d3e3341e6a57254c9
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-10-28 23:32:26 +0000

    Function that takes an object & returns the first key for which the callback returns a truthy value

---
+++ findKey.js
@@ -0,0 +1,43 @@
+const findKey = (obj, cb) => {
+  let kee = undefined;
+
+  for (let item in obj) {
+    if (cb(obj[item])) {
+      return item;
+    }
+  }
+  return kee;
+};
+
+// Assertion test
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Test case 1
+const test1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 20);
+
+assertEqual(test1, undefined);
+
+// Test case 2
+const test2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3);
+
+assertEqual(test2, "Akaleri");
