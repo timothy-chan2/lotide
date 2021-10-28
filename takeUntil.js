f57901084a453d62d92f8f5d9505a2479067765d
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-10-28 22:40:30 +0000

    Function returns a slice of the array with elements taken from the beginning until condition is false

---
+++ takeUntil.js
@@ -0,0 +1,69 @@
+// Function returns a slice of the array with elements taken from the beginning & should keep going until the callback returns a truthy value
+const takeUntil = function(array, callback) {
+  let part = [], i = 0;
+
+  while (callback(array[i]) === false && i < array.length) {
+    if (part) {
+      part.push(array[i]);
+    } else {
+      part = [array[0]];
+    }
+    i++;
+  }
+  return part;
+};
+
+// Assertion functions
+const eqArrays = (arr1, arr2) => {
+  let match = true;
+
+  // Check if both arrays have the same length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Check elements in the same position in each array are equal
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) {
+      match = false;
+    }
+  }
+  return match;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  // Display text element in array surounded by ""
+  const displayArrayExactly = (array) => {
+    let arr = '';
+
+    for (let ind in array) {
+      if (typeof array[ind] === 'string') {
+        arr += `\"${array[ind]}\"`;
+      } else {
+        arr += `${array[ind]}`;
+      }
+      if (ind < array.length - 1) {
+        arr += ', ';
+      }
+    }
+    return arr;
+  };
+
+  // Print out assertion message
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: [${displayArrayExactly(arr1)}] === [${displayArrayExactly(arr2)}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${displayArrayExactly(arr1)}] !== [${displayArrayExactly(arr2)}]`);
+  }
+};
+
+// Test cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < -1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2, -1, 2, 4, 5]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
