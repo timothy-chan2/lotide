13a5fe4d7322e799991bc2f67ae10cbe9ce57616
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-01 19:39:59 +0000

    Update to use mocha & chai for testing instead of customs tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,24 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// Test code
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, "2", 3, 4]), ["2", 3]);
+describe("#tail", () => {
+  it("returns an empty array when array has only 1 element", () => {
+    const arr = [1];
+    assert.deepEqual(middle(arr), []);
+  });
+
+  it("returns an empty array when array has 2 elements", () => {
+    const arr = [1, 2];
+    assert.deepEqual(middle(arr), []);
+  });
+
+  it("returns the middle 2 elements in an array when the array length is an even number", () => {
+    const arr = [1, 2, 3, 4];
+    assert.deepEqual(middle(arr), [2, 3]);
+  });
+
+  it("returns the middle element in an array when the array length is an odd number", () => {
+    const arr = [1, 2, 3];
+    assert.deepEqual(middle(arr), [2]);
+  });
+});
