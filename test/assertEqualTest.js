44b75ad1958a8e4cba85825b09b3d6ab956e13d2
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-01 03:55:18 +0000

    Add test file with test code & importing function associated with the test

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 5);
