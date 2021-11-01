1f1cbb906047b8ad816c70e5ef1851c9a89cbe58
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-01 19:12:45 +0000

    Update to use mocha & chai for testing instead of custom tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns length that is 1 less than the original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(words).length, 2);
+  });
 
-assertEqual(tail(words).length, 2);
-assertEqual(tail(words)[0], "Lighthouse");
-assertEqual(tail(words)[1], "Labs");
+  it("returns an array without element 0 of the original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
+  });
+});
