8ba9e410c2de739c3362618616dca1cf3bb730d1
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-01 13:45:57 +0000

    Move test code into JS file in test folder & module.exports the function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,4 @@
-const eqArrays = (arr1, arr2) => {
-  let match = true;
-
-  // Check if both arrays have the same length
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  // Check elements in the same position in each array are equal
-  for (let i in arr1) {
-    if (arr1[i] !== arr2[i]) {
-      match = false;
-    }
-  }
-  return match;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (arr1, arr2) => {
   // Display text element in array surounded by ""
@@ -41,7 +26,4 @@ const assertArraysEqual = (arr1, arr2) => {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
