d9ca3db3ba3e78f3af9cabc70e15f217b3de74b0
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-02-15 02:05:06 +0000

    Refactor so other functions are imported instead of defined directly in the file

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,27 +1,4 @@
-const eqArrays = require('./eqArrays');
-
-// Returns true if both objects have identical keys with identical values
-const eqObjects = (obj1, obj2) => {
-  const obj1Keys = Object.keys(obj1);
-  const obj2Keys = Object.keys(obj2);
-
-  if (obj1Keys.length === obj2Keys.length) {
-    for (let item in obj1) {
-      if (Array.isArray(obj1[item]) && Array.isArray(obj2[item])) {
-        if (eqArrays(obj1[item], obj2[item]) === false) {
-          return false;
-        }
-      } else {
-        if (obj1[item] !== obj2[item]) {
-          return false;
-        }
-      }
-    }
-  } else {
-    return false;
-  }
-  return true;
-};
+const eqObjects = require('./eqObjects');
 
 // Assert if 2 objects are the same
 const assertObjectsEqual = function(actual, expected) {
--- eqObjects.js
+++ eqObjects.js
@@ -1,19 +1,5 @@
-const eqArrays = (arr1, arr2) => {
-  let match = true;
-
-  // Check if both arrays have the same length
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  // Check elements in the same position in each array are equal
-  for (let i in arr1) {
-    if (arr1[i] !== arr2[i]) {
-      match = false;
-    }
-  }
-  return match;
-};
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
 
 // Returns true if both objects have identical keys with identical values
 const eqObjects = (obj1, obj2) => {
@@ -39,14 +25,6 @@ const eqObjects = (obj1, obj2) => {
 };
 
 // Test code
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
@@ -60,3 +38,5 @@ const cd2 = { c: "1", d: ["2", 3, 4] };
 
 assertEqual(eqObjects(cd, dc), true);
 assertEqual(eqObjects(cd, cd2), false);
+
+module.exports = eqObjects;
