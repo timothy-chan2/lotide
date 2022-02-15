1924101e0a7917b13cb12744b9b9cf8736069502
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-02-15 01:44:45 +0000

    Import the eqArrays function instead of defining the function directly in the file

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,19 +1,4 @@
-const eqArrays = (arr1, arr2) => {
-  let match = true;
-
-  // Check if both arrays have the same length
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  // Check elements in the same position in each array are equal
-  for (let i in arr1) {
-    if (arr1[i] !== arr2[i]) {
-      match = false;
-    }
-  }
-  return match;
-};
+const eqArrays = require('./eqArrays');
 
 // Returns true if both objects have identical keys with identical values
 const eqObjects = (obj1, obj2) => {
