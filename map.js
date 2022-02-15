c5b20578185630d60ebb16a54539da8a22f9b7a8
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-02-15 04:23:20 +0000

    Refactor so additional functions are imported instead of defined directly in the file to keep code DRY

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+const assertArraysEqual = require('./assertArraysEqual');
+
 const map = (arr, cb) => {
   const results = [];
 
@@ -5,55 +7,6 @@ const map = (arr, cb) => {
     results.push(cb(item));
   }
   return results;
-};
-/*
-const words = ["ground", "control", "to", "major", "tom"];
-
-const results1 = map(words, word => word[0]);
-console.log(results1);*/
-
-// Needed to assert the tests
-const eqArrays = (arr1, arr2) => {
-  let match = true;
-
-  // Check if both arrays have the same length
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  // Check elements in the same position in each array are equal
-  for (let i in arr1) {
-    if (arr1[i] !== arr2[i]) {
-      match = false;
-    }
-  }
-  return match;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  // Display text element in array surounded by ""
-  const displayArrayExactly = (array) => {
-    let arr = '';
-
-    for (let ind in array) {
-      if (typeof array[ind] === 'string') {
-        arr += `\"${array[ind]}\"`;
-      } else {
-        arr += `${array[ind]}`;
-      }
-      if (ind < array.length - 1) {
-        arr += ', ';
-      }
-    }
-    return arr;
-  };
-
-  // Print out assertion message
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: [${displayArrayExactly(arr1)}] === [${displayArrayExactly(arr2)}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${displayArrayExactly(arr1)}] !== [${displayArrayExactly(arr2)}]`);
-  }
 };
 
 // Test case 1: square roots
--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,5 @@
+const assertArraysEqual = require('./assertArraysEqual');
+
 // Function returns a slice of the array with elements taken from the beginning & should keep going until the callback returns a truthy value
 const takeUntil = function(array, callback) {
   let part = [], i = 0;
@@ -11,50 +13,6 @@ const takeUntil = function(array, callback) {
     i++;
   }
   return part;
-};
-
-// Assertion functions
-const eqArrays = (arr1, arr2) => {
-  let match = true;
-
-  // Check if both arrays have the same length
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  // Check elements in the same position in each array are equal
-  for (let i in arr1) {
-    if (arr1[i] !== arr2[i]) {
-      match = false;
-    }
-  }
-  return match;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  // Display text element in array surounded by ""
-  const displayArrayExactly = (array) => {
-    let arr = '';
-
-    for (let ind in array) {
-      if (typeof array[ind] === 'string') {
-        arr += `\"${array[ind]}\"`;
-      } else {
-        arr += `${array[ind]}`;
-      }
-      if (ind < array.length - 1) {
-        arr += ', ';
-      }
-    }
-    return arr;
-  };
-
-  // Print out assertion message
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: [${displayArrayExactly(arr1)}] === [${displayArrayExactly(arr2)}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${displayArrayExactly(arr1)}] !== [${displayArrayExactly(arr2)}]`);
-  }
 };
 
 // Test cases
